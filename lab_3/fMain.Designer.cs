e5c0cc8b9e71ef6ff3cc1e2910a87f45039cfede
sokolKS <84147982+sokolKS@users.noreply.github.com>
2021-05-14 21:37:01 +0300

    Add files via upload

---
+++ lab_3/fMain.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace lab_3
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbX1 = new System.Windows.Forms.TextBox();
+            this.tbX2 = new System.Windows.Forms.TextBox();
+            this.tbY = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbA = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCalculate.Location = new System.Drawing.Point(400, 6);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(100, 30);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Обчислити";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(400, 46);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 30);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Очистити";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(400, 86);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 30);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Вихід";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Змінна X1";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(210, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Змінна X2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(167, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Результат розрахунку Y";
+            // 
+            // tbX1
+            // 
+            this.tbX1.Location = new System.Drawing.Point(100, 10);
+            this.tbX1.Name = "tbX1";
+            this.tbX1.Size = new System.Drawing.Size(70, 22);
+            this.tbX1.TabIndex = 6;
+            // 
+            // tbX2
+            // 
+            this.tbX2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbX2.Location = new System.Drawing.Point(290, 10);
+            this.tbX2.Name = "tbX2";
+            this.tbX2.Size = new System.Drawing.Size(70, 22);
+            this.tbX2.TabIndex = 7;
+            // 
+            // tbY
+            // 
+            this.tbY.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbY.Location = new System.Drawing.Point(240, 50);
+            this.tbY.Name = "tbY";
+            this.tbY.Size = new System.Drawing.Size(120, 22);
+            this.tbY.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(200, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Середне арифметичне x1, x2";
+            // 
+            // tbA
+            // 
+            this.tbA.Location = new System.Drawing.Point(240, 90);
+            this.tbA.Name = "tbA";
+            this.tbA.Size = new System.Drawing.Size(120, 22);
+            this.tbA.TabIndex = 10;
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chart1.BackColor = System.Drawing.SystemColors.Control;
+            this.chart1.BorderSkin.PageColor = System.Drawing.Color.Transparent;
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            this.chart1.Location = new System.Drawing.Point(0, 122);
+            this.chart1.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Name = "Sin";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(529, 287);
+            this.chart1.TabIndex = 11;
+            this.chart1.Text = "chart1";
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 409);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.tbA);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbY);
+            this.Controls.Add(this.tbX2);
+            this.Controls.Add(this.tbX1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.MaximizeBox = false;
+            this.Name = "fMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Лабораторна робота №3";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbX1;
+        private System.Windows.Forms.TextBox tbX2;
+        private System.Windows.Forms.TextBox tbY;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbA;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ lab_3/fMain.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace lab_3
+{
+    public partial class fMain : Form
+    {
+        public fMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbX1.Text) ||
+                string.IsNullOrEmpty(tbX2.Text)) {
+                tbY.Text = "Не введено даних!";
+                return;
+            }
+            double x1 = double.Parse(tbX1.Text);
+            double x2 = double.Parse(tbX2.Text);
+            double aver = (x1 + x2) / 2;
+            double y = Math.Sin((x1 - x2) / Math.Pow(x1 + 53 * x2, 2));
+            this.chart1.Series[0].Points.AddXY(aver, y);
+            tbY.Text = y.ToString("0.####");
+            tbA.Text = aver.ToString();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            tbY.Text = string.Empty;
+            tbA.Text = string.Empty;
+            tbX1.Text = string.Empty;
+            tbX2.Text = string.Empty;
+            this.chart1.Series[0].Points.Clear();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
